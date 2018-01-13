445da913406d67f0c3182a339b08354a655d8137
Tomasz Kassa <tomasz.kassa@gmail.com>
2018-01-13 14:22:03 +0100

    Created simple Client based on TCP.

--- Communicator/Client/Client.cs
+++ Communicator/Client/Client.cs
@@ -1,6 +1,44 @@
-﻿public class Client
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net.Sockets;
+
+
+public class Client
 {
     public static void Main(string[] args)
     {
+        try
+        {
+            TcpClient tcp_client = new TcpClient();
+            Console.WriteLine("Connecting.....");
+
+            tcp_client.Connect("192.168.0.229", 8001);
+
+            Console.WriteLine("Connected");
+            Console.Write("Enter the string to be transmitted : ");
+
+            String message = Console.ReadLine();
+            Stream network_stream = tcp_client.GetStream();
+
+            ASCIIEncoding encoder = new ASCIIEncoding();
+            byte[] write_buffer = encoder.GetBytes(message);
+            Console.WriteLine("Transmitting.....");
+
+            network_stream.Write(write_buffer, 0, write_buffer.Length);
+
+            byte[] read_buffer = new byte[100];
+            int data_received = network_stream.Read(read_buffer, 0, 100);
+
+            for (int i = 0; i < data_received; i++)
+                Console.Write(Convert.ToChar(read_buffer[i]));
+
+            tcp_client.Close();
+        }
+
+        catch (Exception e)
+        {
+            Console.WriteLine("Error..... " + e.StackTrace);
+        }
     }
 }
